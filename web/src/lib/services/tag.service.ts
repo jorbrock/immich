863271d5410cbea5769a1dea852621f76e030bc9
jorbrock <brockjord@gmail.com>
2026-04-09 16:18:03 -0700

    Ensure toast displays updated tag name

--- web/src/lib/services/tag.service.ts
+++ web/src/lib/services/tag.service.ts
@@ -60,8 +60,10 @@ export const handleUpdateTag = async (tag: TreeNode, dto: TagUpdateDto) => {
 
   try {
     const response = await updateTag({ id: tag.id, tagUpdateDto: dto });
+    console.log('tagzz');
+    console.log(response);
 
-    toastManager.primary($t('tag_updated', { values: { tag: tag.value } }));
+    toastManager.primary($t('tag_updated', { values: { tag: response?.value || tag.value } }));
     eventManager.emit('TagUpdate', response);
 
     return true;
